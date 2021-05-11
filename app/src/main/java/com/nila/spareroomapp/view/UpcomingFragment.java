e725a18d7b068ab79ca51626703da20537ca9092
Nilavu <gs.nilavazhahi@gmail.com>
2021-05-11 11:41:09 +0100

    2. Used View Binding Instead of Butterknife

--- app/src/main/java/com/nila/spareroomapp/view/UpcomingFragment.java
+++ app/src/main/java/com/nila/spareroomapp/view/UpcomingFragment.java
@@ -11,16 +11,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.nila.spareroomapp.R;
+import com.nila.spareroomapp.databinding.FragmentUpcomingBinding;
 import com.nila.spareroomapp.model.DateList;
 import com.nila.spareroomapp.model.DetailList;
 import com.nila.spareroomapp.model.UpcomingList;
@@ -33,68 +29,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class UpcomingFragment extends Fragment {
 
     private Context context;
 
-    RecyclerView upcomingEventList;
-
-    LinearLayout loadingLayout;
-    LinearLayout emptyLayout;
-    LinearLayout errorLayout;
-    LinearLayout noConnectionLayout;
-    Button offlineRetryBtn, errorRetryBtn;
-
     private ListUpcomingViewModel listViewModel;
     private UpcomingEventListAdapter upcomingEventListAdapter = new UpcomingEventListAdapter(new ArrayList<>());
     List<UpcomingList> upcomingConsolidatedList = new ArrayList<>();
 
+    private FragmentUpcomingBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+        binding = FragmentUpcomingBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
         this.context = getContext();
-//        ButterKnife.bind(context, view);
-
-        loadingLayout = view.findViewById(R.id.loading_layout);
-        emptyLayout = view.findViewById(R.id.empty_layout);
-        errorLayout = view.findViewById(R.id.error_layout);
-        errorRetryBtn = view.findViewById(R.id.error_retry_btn);
-        noConnectionLayout = view.findViewById(R.id.no_connection_layout);
-        offlineRetryBtn = view.findViewById(R.id.offline_retry_btn);
-        upcomingEventList = view.findViewById(R.id.upcomingEventList);
 
         loadData();
 
-        errorRetryBtn.setOnClickListener(new View.OnClickListener() {
+        binding.errorRetryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadData();
             }
         });
 
-        offlineRetryBtn.setOnClickListener(new View.OnClickListener() {
+        binding.offlineRetryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadData();
             }
         });
-         upcomingEventList.setOnClickListener(new View.OnClickListener() {
+
+        binding.upcomingEventList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String phone= "07466887291";
@@ -102,7 +80,6 @@ public class UpcomingFragment extends Fragment {
 
                 intent.setData(Uri.parse("tel:"+phone ));
                 context.startActivity(intent);
-
             }
         });
     }
@@ -113,15 +90,15 @@ public class UpcomingFragment extends Fragment {
             listViewModel = ViewModelProviders.of(this).get(ListUpcomingViewModel.class);
             listViewModel.refresh();
 
-            upcomingEventList.setLayoutManager(new LinearLayoutManager(context));
-            upcomingEventList.setAdapter(upcomingEventListAdapter);
+            binding.upcomingEventList.setLayoutManager(new LinearLayoutManager(context));
+            binding.upcomingEventList.setAdapter(upcomingEventListAdapter);
 
             observerViewModel();
         }else{
-            errorLayout.setVisibility(View.GONE);
-            emptyLayout.setVisibility(View.GONE);
-            upcomingEventList.setVisibility(View.GONE);
-            noConnectionLayout.setVisibility(View.VISIBLE);
+            binding.errorLayout.setVisibility(View.GONE);
+            binding.emptyLayout.setVisibility(View.GONE);
+            binding.upcomingEventList.setVisibility(View.GONE);
+            binding.noConnectionLayout.setVisibility(View.VISIBLE);
 
         }
 
@@ -174,12 +151,12 @@ public class UpcomingFragment extends Fragment {
                         }
                     }
 
-                    upcomingEventList.setVisibility(View.VISIBLE);
+                    binding.upcomingEventList.setVisibility(View.VISIBLE);
                     upcomingEventListAdapter.updateUpcomingEvents(upcomingConsolidatedList);
                 }else{
-                    emptyLayout.setVisibility(View.VISIBLE);
-                    loadingLayout.setVisibility(View.VISIBLE);
-                    upcomingEventList.setVisibility(View.GONE);
+                    binding.emptyLayout.setVisibility(View.VISIBLE);
+                    binding.loadingLayout.setVisibility(View.VISIBLE);
+                    binding.upcomingEventList.setVisibility(View.GONE);
                 }
             }
         });
@@ -188,12 +165,12 @@ public class UpcomingFragment extends Fragment {
             @Override
             public void onChanged(Boolean isError) {
                 if(isError !=null){
-                    errorLayout.setVisibility(isError ? View.VISIBLE : View.GONE);
+                    binding.errorLayout.setVisibility(isError ? View.VISIBLE : View.GONE);
                 }
                 if(isError){
-                    errorLayout.setVisibility(View.VISIBLE);
-                    emptyLayout.setVisibility(View.GONE);
-                    loadingLayout.setVisibility(View.VISIBLE);
+                    binding.errorLayout.setVisibility(View.VISIBLE);
+                    binding.emptyLayout.setVisibility(View.GONE);
+                    binding.loadingLayout.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -202,14 +179,14 @@ public class UpcomingFragment extends Fragment {
             @Override
             public void onChanged(Boolean isLoading) {
                 if (isLoading != null){
-                    loadingLayout.setVisibility( isLoading ? View.VISIBLE : View.GONE);
+                    binding.loadingLayout.setVisibility( isLoading ? View.VISIBLE : View.GONE);
                 }
 
                 if(isLoading){
-                    emptyLayout.setVisibility(View.GONE);
-                    upcomingEventList.setVisibility(View.GONE);
-                    errorLayout.setVisibility(View.GONE);
-                    noConnectionLayout.setVisibility(View.GONE);
+                    binding.emptyLayout.setVisibility(View.GONE);
+                    binding.upcomingEventList.setVisibility(View.GONE);
+                    binding.errorLayout.setVisibility(View.GONE);
+                    binding.noConnectionLayout.setVisibility(View.GONE);
 
                 }
             }
@@ -259,5 +236,9 @@ public class UpcomingFragment extends Fragment {
 
         return groupedTreeMap;
     }
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
